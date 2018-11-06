8fea1a1bfc81d6e5de1f4fa75b9ab687843eeeb0
Kenichi <kenichixie@gmail.com>
2018-11-06 13:07:36 +0700

    finish initial migration and model

---
+++ database/migrations/2018_11_06_044616_create_products_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('category_id')->unsigned();
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->boolean('is_active');
+            $table->string('image_url');
+            $table->string('slug');
+            $table->string('tokopedia_link')->nullable();
+            $table->string('shopee_link')->nullable();
+            $table->string('bukalapak_link')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
