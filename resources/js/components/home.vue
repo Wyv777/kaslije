a2426c6ccf993c9eaa4edb83611b10af67e7ee04
carissa_li <carissalimantara@gmail.com>
2018-11-15 22:19:38 +0700

    link bg to pages

--- resources/js/components/home.vue
+++ resources/js/components/home.vue
@@ -1,19 +1,19 @@
 <template>
 <section class="section is-marginless is-paddingless">
     <div class="columns">
-        <div class="column is-6" id="bg-1">
+        <a href="/catalog" class="column is-6" id="bg-1">
             <a href="/catalog" class="boxButton">
                 <p class="is-5 gold cursive">Browse</p>
                 <p class="subtitle white">OUR MENU</p>
             </a>
-        </div>
+        </a>
 
-        <div class="column is-6" id="bg-2">
+        <a href="/about" class="column is-6" id="bg-2">
             <a href="/about" class="boxButton">
                 <p class="is-5 gold cursive">Discover</p>
                 <p class="subtitle white">OUR STORY</p>
             </a>
-        </div>
+        </a>
         
     </div>
 </section>
